879de21d07af3ede56d4784a6075739bc5dc9b1a
James Shane <james@souratech.com>
2019-03-09 00:27:38 -0700

    added examples for hire me form

---
+++ src/node/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+var options = {
+  index: "index.html"
+};
+app.get('/example/a', function (req, res) {
+  res.send('Hello from A!')
+})
+app.get('/example/b', function (req, res, next) {
+  console.log('the response will be sent by the next function ...')
+  next()
+}, function (req, res) {
+  res.send('Hello from B!')
+})
+
+var birds = require('./birds')
+
+// ...
+
+app.use('/', birds)
+
+app.use("/", express.static("../../public",options));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
